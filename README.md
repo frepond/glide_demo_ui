873a610690dbacd2fe51ef0c51bbe34a3a041660
Federico C. Repond <frepond@gmail.com>
2019-04-12 10:55:38 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# glide_demo_ui
+
+> Glide demo ui
+
+## Considerations
+
+- The project was scaffolded using [NuxtJs](https://nuxtjs.org).
+- Since I haven't worked with UI lately I've chose Vue just for fun and to try something that it's said to be better than Angular.
+- Even when this project is too big for the task, starting from the ground would have been cumbersome and would ended up with a clumsy project unable to grow properly.
+
+## Build Setup & Run
+
+``` bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
+
+__Warning__: you need the api server running in the same host.
+
+## TODO
+
+- Localization
+- Error handling
+- Server side pagination
+- Style
+- Security
+- Dev/Test/Prod configuration
+- ...
---
+++ pages/accounts.vue
@@ -0,0 +1,202 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center>
+      <template>
+        <div>
+          <v-toolbar flat color="white">
+            <v-toolbar-title>Accounts CRUD</v-toolbar-title>
+            <v-divider
+              class="mx-2"
+              inset
+              vertical
+            ></v-divider>
+            <v-spacer></v-spacer>
+            <v-dialog v-model="dialog" max-width="500px">
+              <template v-slot:activator="{ on }">
+                <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">{{ formTitle }}</span>
+                </v-card-title>
+
+                <v-card-text>
+                  <v-container grid-list-md>
+                    <v-layout wrap>
+                      <v-flex xs12 sm12 md12>
+                        <v-text-field v-model="editedItem.email" label="Email"></v-text-field>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
+                  <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+          <v-data-table
+            :headers="headers"
+            :items="accounts"
+            class="elevation-1"
+          >
+            <template v-slot:items="props">
+              <td class="text-xs-left">{{ props.item.id }}</td>
+              <td class="text-xs-left">{{ props.item.email }}</td>
+              <td class="justify-center layout px-0">
+                <v-icon
+                  small
+                  class="mr-2"
+                  @click="editItem(props.item)"
+                >
+                  edit
+                </v-icon>
+                <v-icon
+                  small
+                  @click="deleteItem(props.item)"
+                >
+                  delete
+                </v-icon>
+              </td>
+            </template>
+            <template v-slot:no-data>
+              <v-btn color="primary" @click="initialize">Reset</v-btn>
+            </template>
+          </v-data-table>
+        </div>
+      </template>
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+  import axios from "axios";
+  export default {
+    data: () => ({
+      dialog: false,
+      headers: [
+        {
+          text: 'Id',
+          align: 'left',
+          sortable: false,
+          value: 'id'
+        },
+        { text: 'Email', value: 'email', align: 'left' },
+      ],
+      api_base: "http://localhost:5000/api/v1",
+      accounts: [],
+      editedIndex: -1,
+      editedItem: {
+        email: ''
+      },
+      defaultItem: {
+        email: ''
+      }
+    }),
+
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+      }
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      }
+    },
+
+    created () {
+      this.initialize()
+    },
+
+    methods: {
+      initialize () {
+        let self = this
+        axios.get(this.api_base + '/accounts')
+        .then(function (response) {
+          // handle success
+          self.accounts = response.data
+        })
+        .catch(e => {
+          if (e.response)
+            console.log(e.response.data)
+          else
+            console.log(e)
+        })
+      },
+
+      editItem (item) {
+        this.editedIndex = this.accounts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        const index = this.accounts.indexOf(item)
+        let self = this
+        confirm('Are you sure you want to delete this item?') &&
+          axios.delete(this.api_base + '/accounts/' + item.id)
+          .then(response => {
+            self.accounts.splice(index, 1)
+          })
+          .catch(e => {
+            if (e.response)
+              alert(e.response.data.message)
+            else
+              console.log(e)
+          })
+      },
+
+      close () {
+        this.dialog = false
+        setTimeout(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        }, 300)
+      },
+
+      save () {
+        let self = this
+        if (this.editedIndex > -1) {
+          axios.put(this.api_base + '/accounts/' + this.editedItem.id, this.editedItem)
+          .then(function(response) {
+            Object.assign(self.accounts[self.editedIndex], self.editedItem)
+          })
+          .catch(e => {
+            if (e.response)
+              alert(e.response.data.message)
+            else
+              console.log(e)
+          })
+        } else {
+          axios.post(this.api_base + '/accounts', this.editedItem)
+          .then(function(response) {
+            let uri = response.headers.location
+            return axios.get(uri)
+          })
+          .then(function(response) {
+            self.accounts.push(response.data)
+          })
+          .catch(e => {
+            if (e.response)
+              alert(e.response.data.message)
+            else
+              console.log(e)
+          })
+        }
+        this.close()
+      }
+    }
+  }
+</script>
